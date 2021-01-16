28af321d82a60c93aa6db1381530c8c1cc2461da
aCube1 <player559001@gmail.com>
2021-01-16 16:41:42 -0300

    Changes: 	1. Lua added 	2. GLFW added 	3. GLAD added 	4. library compile to static

---
+++ include/taida.h
@@ -0,0 +1,11 @@
+#ifndef _TAIDA_H_
+#define _TAIDA_H_
+
+#include <stdbool.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#endif /* _TAIDA_H_ */
