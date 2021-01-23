5bdc1223172cd368ff02636f2d74ca23aa158023
aCube1 <player559001@gmail.com>
2021-01-23 13:52:24 -0300

    Demos folder added, library examples is located inside it

---
+++ demos/open_window/src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "Taida/taida.h"
+
+void update(void);
+void render(void);
+
+void destroy(void);
+
+int main(int argc, char* argv[])
+{
+	taida_options_t options = {
+		.title = "test",
+
+		.width = 800,
+		.height = 600,
+	};
+
+	taida_t *taida = taida_create(options, (taida_callbacks_t) {
+				.update = update,
+				.render = render,
+
+				.destroy = destroy,
+			});
+	while (taida_window_isopen(taida)) {
+		taida_loop(taida);
+	}
+
+	taida_destroy(taida);
+
+	return 0;
+}
+
+void update(void) {}
+void render(void) {}
+
+void destroy(void) {}
