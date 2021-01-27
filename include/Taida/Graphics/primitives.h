32c40b04ba13c0be4bb36748c751032f104d9c7f
aCube1 <player559001@gmail.com>
2021-01-27 14:14:58 -0300

    Working on optimization of primitive drawing

--- include/Taida/Graphics/primitives.h
+++ include/Taida/Graphics/primitives.h
@@ -5,6 +5,9 @@
 #include "Taida/System/core.h"
 #include "Taida/Window/window.h"
 
+unsigned int taida_create_shaderProgram(const char* vertexShaderSource, const char *fragmentShaderSource);
+unsigned int taida_create_VAO();
+
 void taida_draw_triangle(taida_t *taida, float x, float y, float width, float height);
 
 #endif /* _PRIMITIVES_H_ */
--- src/Taida/Graphics/primitives.c
+++ src/Taida/Graphics/primitives.c
@@ -1,69 +1,36 @@
 #include "Taida/Graphics/primitives.h"
 
-void taida_draw_triangle(taida_t *taida, float x, float y, float width, float height)
+unsigned int taida_create_shaderProgram(const char *vertexShaderSource, const char *fragmentShaderSource)
 {
 	int success;
-	char infolog[512];
-
-	int window_width, window_height;
-	taida_getwindow_size(taida, &window_width, &window_height);
-
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  	0.5f, 0.0f,
-	};
-
-	unsigned int vertexShader;
-	unsigned int fragmentShader;
-	unsigned int shaderProgram;
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	const char *vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-		"}\0";
-
+	char infoLog[512];
+	
+	unsigned int vertexShader, fragmentShader, shaderProgram;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-	
+
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
-		taida_log_error(infolog);
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		taida_log_error(infoLog);
 		
 		glDeleteShader(vertexShader);
-		return;
+		return 0;
 	}
-
-	const char *fragmentShaderSource = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
-		"}\0";
+	
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
-		taida_log_error(infolog);
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		taida_log_error(infoLog);
 		
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
-		return;
+		return 0;
 	}
 
 	shaderProgram = glCreateProgram();
@@ -74,19 +41,69 @@ void taida_draw_triangle(taida_t *taida, float x, float y, float width, float he
 
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
-		taida_log_error(infolog);
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		taida_log_error(infoLog);
 		
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
+		return 0;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+}
+
+unsigned int taida_create_VAO(){
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	return VAO;
+}
+
+void taida_draw_triangle(taida_t *taida, float x, float y, float width, float height)
+{
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f, 	0.5f, 0.0f,
+	};
+
+	unsigned int shaderProgram, VAO;
+
+	VAO = taida_create_VAO();
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+		"}\0";
+
+	
+	const char *fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+		"}\0";
+
+	shaderProgram = taida_create_shaderProgram(vertexShaderSource, fragmentShaderSource);
+	if (shaderProgram == 0) {
+		taida_log_error("TAIDA - ShaderProgram Can't Be Created");
+
 		return;
 	}
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 	glUseProgram(shaderProgram);
-	
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 	
 	glBindVertexArray(VAO);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
